5fb2512debdbad42fad8169b9345747f7656c857
Gabriel Bealer <bealeg13@gmail.com>
2023-08-31 10:01:15 -0700

    added change

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            //.Add(2, 4);
+            //Methods.Add(2, 4);
             //Methods.Add(3.6, 5.4);
             Console.WriteLine(Methods.Add(2, 3, true));
         }
